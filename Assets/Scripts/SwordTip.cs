a10afc080773fd9057c428c92423c4371a4a9e91
Kailang Fu <kailangfu@gmail.com>
2018-05-26 16:33:49 -0700

    Finish Sword particles

---
+++ Assets/Scripts/SwordTip.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SwordTip : MonoBehaviour {
+	public enum Side {
+		Left,
+		Right,
+	}
+
+	public Side side;
+	public float deltaTime;
+	public Vector3 position, velocity, acceleration;
+	Vector3 lastPosition, lastVelocity;
+
+	const float interval = 0.05f;
+	float lastTime;
+
+	void LateUpdate () {
+		var current = Time.time;
+		//		if (current - lastTime < interval) return;
+		
+		deltaTime = current - lastTime;
+		lastTime = current;
+//		deltaTime = Time.deltaTime;
+
+		position = transform.position;
+		velocity = (position - lastPosition) / deltaTime;
+		acceleration = (velocity - lastVelocity) / deltaTime;
+
+		lastPosition = position;
+		lastVelocity = velocity;
+
+		Debug.DrawRay (position, velocity * 0.1f, Color.green);
+		//Debug.DrawRay(position, acceleration, Color.red);
+	}
+}
