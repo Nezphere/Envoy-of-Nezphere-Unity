f9f3dd18937ace09459fc075b0dc76e3ecdbe054
Kailang Fu <kailangfu@gmail.com>
2018-05-26 17:53:27 -0700

    Add weapon tail

---
+++ Assets/WeaponTrailSpeedToggle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponTrailSpeedToggle : MonoBehaviour {
+	public MeleeWeaponTrail weaponTrail;
+	public SwordTip tip;
+	public float minVelocity = 10;
+
+	void Update () {
+		weaponTrail.Emit = tip.velocity.sqrMagnitude > minVelocity;
+	}
+}
